6160f1d083b06f9331cbabeb963829a00ad76585
JUsmaa <julian.usma09@gmail.com>
2024-08-20 08:14:01 -0500

    se agrega condicionales simples compuestos y anidados

---
+++ script.js
@@ -0,0 +1,11 @@
+let edad = 17
+if(edad > 18){
+    console.log("es mayor de 18")
+
+}
+else if (edad == 18) {
+    console.log("apenas cumplio los 18")
+}
+else{
+    console.log("es menor de edad")
+}
